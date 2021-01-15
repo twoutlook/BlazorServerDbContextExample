f9b8324cb67409b3cb3abf94e366e93202f44d55
Mark Chenpingling <chenpingling@gmail.com>
2021-01-15 11:48:43 +0800

    a Lab for ref

---
+++ BlazorServerEFCoreSample/Lab/Shared/AppState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab.Shared
+{
+    public class AppState
+    {
+        public string SelectedColour { get; private set; }
+
+        public event Action OnChange;
+
+        public void SetColour(string colour)
+        {
+            SelectedColour = colour;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+        
+    }
+}
