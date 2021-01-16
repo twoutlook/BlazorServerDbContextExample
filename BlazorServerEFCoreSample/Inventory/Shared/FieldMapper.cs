623979b77d8c855dc86448a291fddc6cc009a639
Mark Chenpingling <chenpingling@gmail.com>
2021-01-17 07:52:32 +0800

    add back FieldMapper and place it to shared

---
+++ BlazorServerEFCoreSample/Inventory/Shared/FieldMapper.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+
+namespace Inventory.Shared
+{
+    public partial class FieldMapper
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+
+    }
+}
