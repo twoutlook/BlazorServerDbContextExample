eb6ff8536f35de7a3ad58d7c992e9100f08c24b8
Mark Chenpingling <chenpingling@gmail.com>
2021-01-18 21:14:08 +0800

    quick fix for CMD MST SORTING

---
+++ BlazorServerEFCoreSample/Inventory/Grid/BaseFiltersV2.cs
@@ -0,0 +1,85 @@
+﻿using Inventory.Shared;
+using System;
+
+namespace Inventory.Grid
+{
+    /// <summary>
+    /// State of grid filters.
+    /// </summary>
+
+    //public class ParameterFilters : IBaseFilters
+    // public class BaseFilters : IBaseFilters
+    public class BaseFiltersV2 : IBaseFiltersV2
+    {
+        //  private const int V = 10;
+
+        /// <summary>
+        /// Keep state of paging.
+        /// </summary>
+        public IPageHelper PageHelper { get; set; }
+
+        //public LocationFilters(IPageHelper pageHelper)
+        public BaseFiltersV2()
+
+        {
+
+            //PageHelper = pageHelper;
+
+            // NOTE by Mark, 2021-01-14
+            // 直接使用元件
+
+            PageHelper = new PageHelper();
+
+            PageHelper.PageSize = 10;
+            PageHelper.BaseUrl = "/base/"; // NOTE by Mark, 解決了可以共同  PageHelper, 可以各自定 PageSize 和 BaseUrl
+
+
+        }
+
+        /// <summary>
+        /// Avoid multiple concurrent requests.
+        /// </summary>
+        public bool Loading { get; set; }
+
+
+        /// <summary>
+        /// Column to sort by.
+        /// </summary>
+        public ApplicationFilterColumns SortColumn { get; set; }
+
+
+        /// <summary>
+        /// True when sorting ascending, otherwise sort descending.
+        /// </summary>
+        public bool SortAscending { get; set; } = true;
+
+        /// <summary>
+        /// Column filtered text is against.
+        /// </summary>
+        public ApplicationFilterColumns FilterColumn { get; set; }
+
+
+
+        public DateTime FilterDt1 { get; set; }
+        public DateTime FilterDt2 { get; set; }
+
+        /// <summary>
+        /// Text to filter on.
+        /// </summary>
+        public string FilterText { get; set; }
+        public string FilterTextF1 { get; set; }
+        public string FilterTextF2 { get; set; }
+        public string FilterTextF3 { get; set; }
+        public string FilterTextF4 { get; set; }
+        public string FilterTextF5 { get; set; }
+        public string FilterTextF6 { get; set; }
+        public string FilterTextF7 { get; set; }
+        //public string FilterLastName { get; set ; }
+
+        public string WorkWith { get; set; }
+        public string ErrMsg { get; set; }
+        public ApplicationFilterColumns DefaultColumn { get; set; }
+        public AppSortType SortType { get; set; }
+
+    }
+}
--- BlazorServerEFCoreSample/Inventory/Grid/Q011VCmdMstGridQueryAdapter.cs
+++ BlazorServerEFCoreSample/Inventory/Grid/Q011VCmdMstGridQueryAdapter.cs
@@ -14,14 +14,15 @@ namespace Inventory.Grid
     //   public class Q006OutbillGridQueryAdapter
     // public class Q008OutbillDGridQueryAdapter
     // public class Q009VCmdMstGridQueryAdapter
-   // public class Q010VCmdMstGridQueryAdapter
+    // public class Q010VCmdMstGridQueryAdapter
     public class Q011VCmdMstGridQueryAdapter
     {
         /// <summary>
         /// Holds state of the grid.
         /// </summary>
         //private readonly ILocationFilters _controls;
-        private readonly IBaseFilters _controls;
+        private readonly IBaseFiltersV2 _controls;
+        public IBaseFiltersV2 f;
 
 
         /// <summary>
@@ -35,58 +36,148 @@ namespace Inventory.Grid
                 ////{ ApplicationFilterColumns.Memo, c => c.Memo },
                 //{ ApplicationFilterColumns.FlagId, c => c.FlagId },
                 { ApplicationFilterColumns.CmdSno, c => c.CmdSno },
+             
+
                 //{ ApplicationFilterColumns.Cinvname, c => c.Cinvname },
                 //{ ApplicationFilterColumns.Iqty, c => c.Iqty },
 
             };
 
+        private readonly Dictionary<ApplicationFilterColumns, Expression<Func<VCmdMst, int>>> _expressionsInt
+            = new Dictionary<ApplicationFilterColumns, Expression<Func<VCmdMst, int>>>
+            {
+             
+                { ApplicationFilterColumns.WmsTskId, c => c.WmsTskId },
+
+        
+            };
+
+
+
         /// 
         private readonly string FilterTextF1;
 
-        public IBaseFilters f;
-
+        
+        // NOTE by Mark, 2021-01-18
+        private ApplicationFilterColumns DEFAULT_SORT_COL =  ApplicationFilterColumns.CmdSno;
+        
+       
         //   public PartGridQueryAdapter(ILocationFilters controls)
         //public Q006OutbillGridQueryAdapter(IBaseFilters controls)
-            public Q011VCmdMstGridQueryAdapter()
-            {
+        public Q011VCmdMstGridQueryAdapter()
+        {
             //    _controls = controls;
-            _controls = new BaseFilters();
+            _controls = new BaseFiltersV2(); // 
             f = _controls;
             _controls.PageHelper.BaseUrl = "/vcmdmst/";
 
-            _controls.FilterColumn = ApplicationFilterColumns.CmdSno;
-            _controls.SortColumn = ApplicationFilterColumns.CmdSno;
-     
+            //_controls.FilterColumn = ApplicationFilterColumns.CmdSno;
+            //_controls.SortColumn = ApplicationFilterColumns.CmdSno;
+            _controls.FilterColumn = DEFAULT_SORT_COL;
+            //      _controls.SortColumn = DEFAULT_SORT_COL; // NOTE by Mark, 因為 string or int , 不能混用
+
+            // make it as default, for most of the cases
+//            f.SortType = AppSortType.TYPE_STR;
+            f.SortType = AppSortType.TYPE_INT;   // default as WmsTskId
+
+            _controls.DefaultColumn = ApplicationFilterColumns.WmsTskId;
+            _controls.SortColumn = ApplicationFilterColumns.WmsTskId;
+
+
         }
 
 
 
         public async Task<ICollection<VCmdMst>> FetchAsyncV4(IQueryable<VCmdMst> query)
         {
-     
+
 
             if (!string.IsNullOrWhiteSpace(_controls.FilterTextF1))
             {
                 //query = query.Where(x => x.Caccountid == _controls.FilterTextF1);
-                query = query.Where(x => x.CmdSno.Contains(_controls.FilterTextF1));
+                query = query.Where(x => x.Loc.Contains(_controls.FilterTextF1));
+
+            }
+
+            if (!string.IsNullOrWhiteSpace(_controls.FilterTextF2))
+            {
+                //query = query.Where(x => x.Caccountid == _controls.FilterTextF1);
+                query = query.Where(x => x.Cticketcode.Contains(_controls.FilterTextF2));
+
+            }
+
+            if (!string.IsNullOrWhiteSpace(_controls.FilterTextF3))
+            {
+                //query = query.Where(x => x.Caccountid == _controls.FilterTextF1);
+                query = query.Where(x => x.Remark.Contains(_controls.FilterTextF3));
 
             }
 
 
+
             // NOTE by Mark, 2021-01-18, 必需要有指定的預設排序欄位
             // 如果沒有, 會報錯, 是不是可能 智能指定一個?
-
-            var expression = _expressions[_controls.SortColumn];
-  
-
+            // System.Collections.Generic.KeyNotFoundException: The given key 'Code' was not present in the dictionary.
+            //         at System.Collections.Generic.Dictionary`2.get_Item(TKey key)
+            //at Inventory.Grid.Q011VCmdMstGridQueryAdapter.FetchAsyncV4(IQueryable`1 query) in 
+            //  var expression = _expressions[_controls.SortColumn];
 
 
 
             var sortDir = _controls.SortAscending ? "ASC" : "DESC";
-    
+            f.ErrMsg = "";
+            if (f.SortType == AppSortType.TYPE_STR)
+            {
+                var expression = _expressions[ApplicationFilterColumns.CmdSno];
+               
+                if (_expressions.ContainsKey(_controls.SortColumn))
+                {
+                    expression = _expressions[_controls.SortColumn];
+                }
+                else
+                {
+                    // the key doesn't exist.
+                    // NOTE by Mark, 2021-01-18, how to feedback to page for developer
+                    // to fix this wrong sorting?
+                    f.ErrMsg = " *** 開發人員請注意見 *** 使用的排序欄位, 還沒有在排序的 dictionary (string) 裡定義!";
+                }
 
-            query = _controls.SortAscending ? query.OrderBy(expression)
-                : query.OrderByDescending(expression);
+
+
+
+                query = _controls.SortAscending ? query.OrderBy(expression)
+                    : query.OrderByDescending(expression);
+
+            }
+
+            if (f.SortType == AppSortType.TYPE_INT)
+            {
+                var expressionInt = _expressionsInt[ApplicationFilterColumns.WmsTskId];
+             //   f.ErrMsg = "";
+                if (_expressionsInt.ContainsKey(_controls.SortColumn))
+                {
+                    expressionInt = _expressionsInt[_controls.SortColumn];
+                }
+                else
+                {
+                    // the key doesn't exist.
+                    // NOTE by Mark, 2021-01-18, how to feedback to page for developer
+                    // to fix this wrong sorting?
+                    f.ErrMsg = " *** 開發人員請注意見 *** 使用的排序欄位, 還沒有在排序的 dictionary (int) 裡定義!";
+                }
+
+
+                query = _controls.SortAscending ? query.OrderBy(expressionInt)
+                    : query.OrderByDescending(expressionInt);
+            }
+
+
+
+
+
+
+
+
 
 
 
