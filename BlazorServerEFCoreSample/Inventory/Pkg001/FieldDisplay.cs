5576c427ebaf20ed1bbc7d145a14c65ffb149849
Mark Chenpingling <chenpingling@gmail.com>
2021-01-22 20:38:30 +0800

    outasn is done!

---
+++ BlazorServerEFCoreSample/Inventory/Pkg001/FieldDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inventory.Pkg001
+{
+    //https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9
+    public record FieldDisplay
+    {
+        public int FieldIndex { get; init; }
+        public string Display { get; init; }
+
+    }
+}
