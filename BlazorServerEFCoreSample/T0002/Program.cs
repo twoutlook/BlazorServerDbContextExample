9fdc0c4001cfde1d1f60251587e1102147eccd9b
Mark Chenpingling <chenpingling@gmail.com>
2021-01-27 11:25:59 +0800

    able to loop namespace classes and context db set as well

--- BlazorServerEFCoreSample/T0002/Program.cs
+++ BlazorServerEFCoreSample/T0002/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace T0002
@@ -13,9 +14,27 @@ namespace T0002
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            //Type[] typelist = GetTypesInNamespace(Assembly.GetExecutingAssembly(), "MyNamespace");
+            //for (int i = 0; i < typelist.Length; i++)
+            //{
+            //    Console.WriteLine(typelist[i].Name);
+            //}
+            var typelist = GetTypesFromNamespace(Assembly.GetExecutingAssembly(), "T0002.Models");
+            foreach (var x in typelist)
+            {
+                Console.WriteLine(x.Name);
+            }
+            
+// CreateHostBuilder(args).Build().Run();
         }
 
+        public static IEnumerable<Type> GetTypesFromNamespace(Assembly assembly,
+                                               String desiredNamespace)
+        {
+            return assembly.GetTypes()
+                           .Where(type => type.Namespace == desiredNamespace);
+        }
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
