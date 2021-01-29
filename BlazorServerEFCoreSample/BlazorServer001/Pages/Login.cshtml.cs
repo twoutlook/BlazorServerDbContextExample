d493adea0b8a0c16b9ccdb3c76a0a3b23f62af7a
Mark Chenpingling <chenpingling@gmail.com>
2021-01-29 11:13:56 +0800

    @addTagHelper *, Microsoft.AspNetCore.Mvc.TagHelpers working, no layout yet

--- BlazorServerEFCoreSample/BlazorServer001/Pages/Login.cshtml.cs
+++ BlazorServerEFCoreSample/BlazorServer001/Pages/Login.cshtml.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +13,41 @@ namespace BlazorServer001.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
         public void OnGet()
         {
+
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!(Email == "admin@example.com" && Password == "password"))
+            {
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Mark"),
+                new Claim(ClaimTypes.Email, Email),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(
+               CookieAuthenticationDefaults.AuthenticationScheme,
+               new ClaimsPrincipal(claimsIdentity));
+
+            return LocalRedirect(Url.Content("~/"));
+        }
+
     }
 }
