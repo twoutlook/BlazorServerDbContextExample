411000093c0c5ac05e8bc8f91db7dd095df5c3b2
Mark Chenpingling <chenpingling@gmail.com>
2021-01-29 19:15:14 +0800

    able to loop claims

---
+++ BlazorServerEFCoreSample/BlazorServer001/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BlazorServer001.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorServer001.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorServer001.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlazorServer001Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BlazorServer001ContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlazorServer001Context>();
+            });
+        }
+    }
+}
