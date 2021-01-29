48f6536b4c54ac34efdbb4d321e4c367bc73cc69
Mark Chenpingling <chenpingling@gmail.com>
2021-01-29 14:14:15 +0800

    搞定了, 1.input submit instead of btn, 2.__RequestVerificationToken is required

--- BlazorServerEFCoreSample/BlazorServer001/Pages/Logout.cshtml.cs
+++ BlazorServerEFCoreSample/BlazorServer001/Pages/Logout.cshtml.cs
@@ -12,17 +12,24 @@ namespace BlazorServer001.Pages
     public class LogoutModel : PageModel
     {
         //public async Task<IActionResult> OnGetAsync()
+
+        //public async Task<IActionResult> OnPostAsync()
+        //{
+        //    await HttpContext
+        //        .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    return LocalRedirect(Url.Content("~/"));
+        //}
         public async Task<IActionResult> OnPostAsync()
         {
             await HttpContext
                 .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return LocalRedirect(Url.Content("~/"));
         }
-        public async Task<IActionResult> OnGetAsync()
-        {
-            await HttpContext
-                .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return LocalRedirect(Url.Content("~/"));
-        }
+        //public async Task<IActionResult> OnGetAsync()
+        //{
+        //    await HttpContext
+        //        .SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    return LocalRedirect(Url.Content("~/"));
+        //}
     }
 }
