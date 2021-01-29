27a49624c016488fb339fe110cda7fbfd2d21fff
Mark Chenpingling <chenpingling@gmail.com>
2021-01-29 11:35:37 +0800

    AddCookie and UseAuth x 2

--- BlazorServerEFCoreSample/BlazorServer001/Startup.cs
+++ BlazorServerEFCoreSample/BlazorServer001/Startup.cs
@@ -1,4 +1,5 @@
 using BlazorServer001.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,6 +29,9 @@ namespace BlazorServer001
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +49,10 @@ namespace BlazorServer001
             app.UseStaticFiles();
 
             app.UseRouting();
+
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
